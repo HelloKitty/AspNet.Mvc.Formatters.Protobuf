49ac15fe293976584577a4bdb0db5f3b94790829
Andrew Blakely <andrew.blakely@ymail.com>
2016-03-11 23:41:23 -0600

    Add NugetPacking

--- MyGet.bat
+++ MyGet.bat
@@ -1,3 +1,3 @@
 dnvm upgrade
-dnu restore -s https://www.myget.org/F/aspnetvnext/ -f https://www.nuget.org/api/v2/
-dnu build ./src/AspNet.Mvc.Formatters.Protobuf/project.json
+dnu restore
+dnu pack ./src/AspNet.Mvc.Formatters.Protobuf/project.json
